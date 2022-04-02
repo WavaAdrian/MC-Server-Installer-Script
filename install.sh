cb68c9ec7f7385c681e3c0482f461aa295996b80
WFH-Community <87927419+WFH-Community@users.noreply.github.com>
2022-04-02 21:15:35 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PS3='Please enter your choice: '
+options=("Begin The Script" "Quit")
+select opt in "${options[@]}"
+do
+case $opt in
+"Begin The Script")
+bash <(curl -s https://imaged.ml/mcinstallweb.sh)
+;;
+"Quit")
+break
+;;
+*) echo "invalid option $REPLY";;
+esac
+done
